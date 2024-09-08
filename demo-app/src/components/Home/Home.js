6e6057b38a2d6b8253a1384647b55ac8da022852
Santhiya <91111567+SanthiyaSandySandy@users.noreply.github.com>
2024-09-08 19:17:39 +0530

    Update Home.js

--- demo-app/src/components/Home/Home.js
+++ demo-app/src/components/Home/Home.js
@@ -1,12 +1,18 @@
 import React from 'react'
 import './Home.css'
+import {useNavigate} from 'react-router-dom'
 
 const Home = () => {
+
+  const history = useNavigate()
+  const addTask = () => {
+    history("/signin")
+  }
   return (
     <div>
         <div className='home d-flex justify-content-center align-items-center'>
             <div className='container d-flex justify-content-center align-items-center flex-column'>
-                <button className='home-btn p-2'>Add Tasks</button>
+                <button className='home-btn p-2'onClick={addTask}>Add Tasks</button>
             </div>
         </div>
     </div>
