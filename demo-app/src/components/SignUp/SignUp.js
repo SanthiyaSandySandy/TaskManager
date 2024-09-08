d921392280e7786c2d6137073eb208a34241aceb
Santhiya <91111567+SanthiyaSandySandy@users.noreply.github.com>
2024-09-08 20:36:33 +0530

    Update SignUp.js

--- demo-app/src/components/SignUp/SignUp.js
+++ demo-app/src/components/SignUp/SignUp.js
@@ -1,94 +1,121 @@
+const router = require("express").Router()
+const User = require('../models/user');
+const bcrypt = require("bcryptjs")
+const List = require('../models/list');
 
-import React, { useState } from 'react'
-import './SignUp.css'
-import axios from 'axios';
-import { ToastContainer, toast } from "react-toastify";
-import 'react-toastify/dist/ReactToastify.css';
-import {useNavigate} from 'react-router-dom';
-import { useDispatch } from 'react-redux';
-import { authActions } from '../../store';
-import { GoogleLogin} from '@react-oauth/google'
+//add Task
 
-const SignUp = () => {
+router.post('/addTask', async (req, res) => {
+    console.log(req.body)
+    try {
+        const { title, body, id } = req.body;
+        const existingUser = await User.findById(id);
+        if (existingUser) {
+            const list = new List({ title, body, user: existingUser });
+            await list.save().then(() => res.status(200).json({ list }));
+            existingUser.list.push();
+            existingUser.save()
+        }
+    } catch (error) {
+        console.log(error)
+    }
+})
 
-  const history =  useNavigate()
-  const dispatch = useDispatch()
-  const [showGoogleLogin,setShowGoogleLogin]=useState(false)
-  const [inputs, setInputs] = useState({
-    email: "",
-    username:"",
-    password:"",
-    confirmPassword:""
-  })
+// update Task
 
-  const handleChange = (e) => { 
-    const {name,value} = e.target;
-    setInputs({...inputs, [name]:value})
-  }
+router.put('/updateTask/:id', async (req, res) => {
+    console.log(req.body)
+    try {
+        const { title, body } = req.body;
+            const list = await List.findByIdAndUpdate(req.params.id, { title, body });
+            list.save().then(() => res.status(200).json({ message: "Task updated" }))
+    } catch (error) {
+        res.status(200).json({ message: "Unable to Update" })
+    }
+})
 
-  const login = () =>{
-    setShowGoogleLogin(true)
-  }
+//delete task
+router.delete('/deleteTask/:id', async (req, res) => {
+    console.log(req.body)
+    try {
+        const { id } = req.body;
+        const existingUser = await User.findByIdAndUpdate(
+            id,
+            { $pull: { list: req.params.id } }
+        );
+        console.log(existingUser)
+        if (existingUser) {
+            await List.findByIdAndDelete(req.params.id).then(() => res.status(200).json({ message: "Task Deleted" }));
+        } else {
+            res.status(200).json({ message: "Not found" })  
+        }
+    } catch (error) {
+        console.log(error)
+    }
+})
 
-  const handleLoginSuccess = (credentialResponse) => {
-    console.log("Login success", credentialResponse)
-    dispatch(authActions.login())
-        history("/todo")
-  }
+//get task by ID
+router.get("/getTask/:id", async (req, res) => {
+    const list = await List.find({ user: req.params.id }).sort({ createdAt: 1 });
+    if (list.length != 0) {
+        res.status(200).json({ list })
+    } else {
+        res.status(200).json({ message: "No Task Available" })
+    }
+})
 
-  const handleLoginFailure = ()=> {
-    console.log("Login Failed")
-  }
+//get task
+router.get("/getTask", async (req, res) => {
+    const list = await List();
+    if (list.length != 0) {
+        res.status(200).json({ list })
+    } else {
+        res.status(400).json({ message: "no list added" })
+    }
+})
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    await axios.post("http://localhost:1000/api/v1/register", inputs).then((response)=>{
-      // console.log(response);
-      if (response.data.message === "User already exists"){
-        // alert(response.data.message)
-        toast.error(response.data.message)
-      } else {
-        toast.success(response.data.message)
-        // alert(response.data.message)
-        setInputs({
-          email:"",
-          username:"",
-          password:"",
-          confirmPassword:""
-        })
-        history("/signin")
-      }
-    })
-  }
 
-  return (
-    <div className='signup'>
-       <ToastContainer />
-      <div className='container'>
-        <div className='row'>
-          <div className='col-lg-8 column d-flex justify-content-center align-items-center'>
-            <div className='d-flex flex-column w-100 p-5'>
-            <input className="p-2 my-3 input-signup" type='email' name="email" placeholder='Email' value={inputs.email} onChange={handleChange} />
-            <input className="p-2 my-3 input-signup" type='username' name ='username' placeholder='UserName' value={inputs.username} onChange={handleChange}/>
-            <input className="p-2 my-3 input-signup" type='password' name='password'placeholder='Password' value={inputs.password} onChange={handleChange}/>
-            <input className="p-2 my-3 input-signup" type='password'  name='confirmPassword' placeholder='Confirm Password' value={inputs.confirmPassword} onChange={handleChange}/>
-            <button className='btn-signup p-2' onClick={handleSubmit}>Sign Up</button>
-            <h6 className='p-2 my-3 input-signup'>Or</h6>
-            <button className='btn-signup p-2' onClick={login}>Sign-Up With Google</button>
-            {
-              showGoogleLogin && (
-                <GoogleLogin
-                onSuccess={handleLoginSuccess}
-                onError={handleLoginFailure}
-                />
-              )
-            }
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  )
-}
+//Sign Up
 
-export default SignUp
+router.post('/register', async (req, res) => {
+    console.log(req.body)
+    try {
+        const { email, username, password, confirmPassword } = req.body;
+        if (password == confirmPassword) {
+            const hashpassword = bcrypt.hashSync(password)
+            const user = new User({ email, username, password: hashpassword });
+            await user.save().then(() => res.status(200).json({message:"User Added Successfully"}))
+        } else {
+            res.status(400).json({ message: "Password and Confirm Password dosen't match" })
+        }
+    } catch (error) {
+        console.log(error)
+        res.status(200).json({ message: "User already exists" })
+    }
+})
+
+//Sign-In
+
+router.post("/login", async (req, res) => {
+    console.log(req.body)
+    try {
+        const user = await User.findOne({ email: req.body.email });
+        if (!user) {
+            res.status(200).json({ message: "User not found, Please Sign-Up First" })
+            return  
+        }
+        const passwordCorrect = bcrypt.compareSync(req.body.password, user.password);
+     if (!passwordCorrect) {
+            res.status(200).json({ message: "Incorrect password" })
+        } else {
+            const { password, ...others } = user._doc;
+            res.status(200).json({ others })
+        }
+
+    } catch (error) {
+        console.log(error)
+        res.status(200).json({ message: "User already exists" })
+    }
+})
+
+module.exports = router
