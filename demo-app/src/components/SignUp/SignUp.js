3fc32bd225535b866e033a7edecfe7cb56332f85
Santhiya <91111567+SanthiyaSandySandy@users.noreply.github.com>
2024-09-08 17:53:25 +0530

    Update SignUp.js

--- demo-app/src/components/SignUp/SignUp.js
+++ demo-app/src/components/SignUp/SignUp.js
@@ -1,16 +1,18 @@
 
 import React, { useState } from 'react'
 import './SignUp.css'
-import HeadingComp from './HeadingComp';
 import axios from 'axios';
 import { ToastContainer, toast } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
 import {useNavigate} from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { authActions } from '../../store';
 import { useGoogleLogin } from '@react-oauth/google';
 
 const SignUp = () => {
 
   const history =  useNavigate()
+  const dispatch = useDispatch()
   const [inputs, setInputs] = useState({
     email: "",
     username:"",
@@ -23,9 +25,18 @@ const SignUp = () => {
     setInputs({...inputs, [name]:value})
   }
 
+  const login = useGoogleLogin({
+    onSuccess:(tokenResponse) => {
+      // console.log(tokenResponse)
+      toast.success("Signed-Up Success")
+      dispatch(authActions.login())
+        history("/todo")
+    },
+  })
+
   const handleSubmit = async (e) => {
     e.preventDefault();
-    await axios.post("https://taskmanager-finalbackend.onrender.com/api/v1/register", inputs).then((response)=>{
+    await axios.post("http://localhost:1000/api/v1/register", inputs).then((response)=>{
       // console.log(response);
       if (response.data.message === "User already exists"){
         // alert(response.data.message)
@@ -43,14 +54,6 @@ const SignUp = () => {
       }
     })
   }
-    const login = useGoogleLogin({
-    onSuccess:(tokenResponse) => {
-      // console.log(tokenResponse)
-      toast.success("Signed-Up Success")
-      dispatch(authActions.login())
-        history("/todo")
-    },
-  })
 
   return (
     <div className='signup'>
@@ -64,7 +67,7 @@ const SignUp = () => {
             <input className="p-2 my-3 input-signup" type='password' name='password'placeholder='Password' value={inputs.password} onChange={handleChange}/>
             <input className="p-2 my-3 input-signup" type='password'  name='confirmPassword' placeholder='Confirm Password' value={inputs.confirmPassword} onChange={handleChange}/>
             <button className='btn-signup p-2' onClick={handleSubmit}>Sign Up</button>
-                        <h6 className='p-2 my-3 input-signup'>Or</h6>
+            <h6 className='p-2 my-3 input-signup'>Or</h6>
             <button className='btn-signup p-2' onClick={login()}>Sign-Up With Google</button>
             </div>
           </div>
