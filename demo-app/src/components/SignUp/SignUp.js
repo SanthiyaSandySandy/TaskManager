7949f82b0644a776678ba03621d9184726e70c0d
Santhiya <91111567+SanthiyaSandySandy@users.noreply.github.com>
2024-09-08 20:41:19 +0530

    Update SignUp.js

--- demo-app/src/components/SignUp/SignUp.js
+++ demo-app/src/components/SignUp/SignUp.js
@@ -1,22 +1,29 @@
+
 import React, { useState } from 'react'
-import './SignIn.css';
+import './SignUp.css'
 import axios from 'axios';
+import { ToastContainer, toast } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
 import {useNavigate} from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import { authActions } from '../../store';
-import { ToastContainer, toast } from 'react-toastify';
 import { GoogleLogin} from '@react-oauth/google';
 import {jwtDecode} from 'jwt-decode'
 
-const SignIn = () => {
-  const history = useNavigate()
+const SignUp = () => {
+
+  const history =  useNavigate()
   const dispatch = useDispatch()
   const [showGoogleLogin,setShowGoogleLogin]=useState(false)
-  const [inputs, setInputs] = useState({email:"", password:""})
+  const [inputs, setInputs] = useState({
+    email: "",
+    username:"",
+    password:"",
+    confirmPassword:""
+  })
 
-  const handleChange = (e) => {
-    const {name, value} = e.target;
+  const handleChange = (e) => { 
+    const {name,value} = e.target;
     setInputs({...inputs, [name]:value})
   }
 
@@ -24,52 +31,31 @@ const SignIn = () => {
     setShowGoogleLogin(true)
   }
 
-  const handleSubmit =async (e) => {
-    e.preventDefault();
-    await axios.post("http://localhost:1000/api/v1/login", inputs).then((response)=>{
-      console.log(response);
-      if(response.data.message === "User not found, Please Sign-Up First" || response.data.message === "Incorrect password" || response.data.message === "User already exists"){
-        toast.error(response.data.message)
-      } else {
-        sessionStorage.setItem("id", response.data.others._id)
-        dispatch(authActions.login())
-        setInputs({
-          email:"",
-          password:""
-        })
-        console.log(response.data.others._id)
-        history("/todo")
-        toast.success("Sign-In success")
-      }
-    })
-  }
-
   const handleLoginSuccess = async (credentialResponse) => {
-    // console.log("Login success", credentialResponse)
     const decodeToken = jwtDecode(credentialResponse.credential)
-    // console.log('Decoded Token', decodeToken)
-    await axios
-    .post("http://localhost:1000/api/v1/login", {
+    // console.log('Decoded Token', decodeToken.name)
+    await axios.post("http://localhost:1000/api/v1/register",{
       email: decodeToken.email,
-      password:"undefined"
-    })
-    .then((response)=>{
-      console.log(response);
-      if(response.data.message === "User not found, Please Sign-Up First" || response.data.message === "Incorrect password" || response.data.message === "User already exists"){
+      username: decodeToken.name,
+      password:"undefined",
+      confirmPassword:"undefined"
+    }).then((response)=>{
+      // console.log(response);
+      if (response.data.message === "User already exists"){
+        // alert(response.data.message)
         toast.error(response.data.message)
       } else {
-        sessionStorage.setItem("id", response.data.others._id)
-        dispatch(authActions.login())
+        toast.success(response.data.message)
+        // alert(response.data.message)
         setInputs({
           email:"",
-          password:""
+          username:"",
+          password:"",
+          confirmPassword:""
         })
-        console.log(response.data.others._id)
-        history("/todo")
-        toast.success("Sign-In success")
+        history("/signin")
       }
     })
-    // sessionStorage.setItem("id",decodeToken.sub)
     // dispatch(authActions.login())
     //     history("/todo")
   }
@@ -78,18 +64,41 @@ const SignIn = () => {
     console.log("Login Failed")
   }
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    await axios.post("http://localhost:1000/api/v1/register", inputs).then((response)=>{
+      // console.log(response);
+      if (response.data.message === "User already exists"){
+        // alert(response.data.message)
+        toast.error(response.data.message)
+      } else {
+        toast.success(response.data.message)
+        // alert(response.data.message)
+        setInputs({
+          email:"",
+          username:"",
+          password:"",
+          confirmPassword:""
+        })
+        history("/signin")
+      }
+    })
+  }
+
   return (
     <div className='signup'>
-      <ToastContainer/>
+       <ToastContainer />
       <div className='container'>
         <div className='row'>
           <div className='col-lg-8 column d-flex justify-content-center align-items-center'>
             <div className='d-flex flex-column w-100 p-5'>
-            <input className="p-2 my-3 input-signup" type='email' name="email" value={inputs.email} placeholder='Email' onChange={handleChange}/>
-            <input className="p-2 my-3 input-signup" type='password' name="password" value={inputs.password} placeholder='Password' onChange={handleChange}/>
-            <button className='btn-signup p-2' onClick={handleSubmit}>Sign In</button>
+            <input className="p-2 my-3 input-signup" type='email' name="email" placeholder='Email' value={inputs.email} onChange={handleChange} />
+            <input className="p-2 my-3 input-signup" type='username' name ='username' placeholder='UserName' value={inputs.username} onChange={handleChange}/>
+            <input className="p-2 my-3 input-signup" type='password' name='password'placeholder='Password' value={inputs.password} onChange={handleChange}/>
+            <input className="p-2 my-3 input-signup" type='password'  name='confirmPassword' placeholder='Confirm Password' value={inputs.confirmPassword} onChange={handleChange}/>
+            <button className='btn-signup p-2' onClick={handleSubmit}>Sign Up</button>
             <h6 className='p-2 my-3 input-signup'>Or</h6>
-            <button className='btn-signup p-2' onClick={login}>Sign-In With Google</button>
+            <button className='btn-signup p-2' onClick={login}>Sign-Up With Google</button>
             {
               showGoogleLogin && (
                 <GoogleLogin
@@ -106,4 +115,4 @@ const SignIn = () => {
   )
 }
 
-export default SignIn
+export default SignUp
