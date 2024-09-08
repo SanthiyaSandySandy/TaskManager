dd006ed2fb496726a014b6727901384c51840b2c
Santhiya <91111567+SanthiyaSandySandy@users.noreply.github.com>
2024-09-08 19:07:00 +0530

    Update Update.js

--- demo-app/src/components/ToDo/Update.js
+++ demo-app/src/components/ToDo/Update.js
@@ -25,7 +25,7 @@ const Update = ({ display, update }) => {
   const handleSubmit = async () => {
     // console.log(inputs)
     await axios
-      .put(`https://taskmanager-finalbackend.onrender.com/api/v1/updateTask/${update._id}`, inputs)
+      .put(`http://localhost:1000/api/v1/updateTask/${update._id}`, inputs)
       .then((response) => {
         toast.success(response.data.message)
         console.log(response)
