71473809837464cb4eb6b5c33fe3e14e757ecb90
Santhiya <91111567+SanthiyaSandySandy@users.noreply.github.com>
2024-09-08 17:39:02 +0530

    Update index.js

--- demo-app/src/index.js
+++ demo-app/src/index.js
@@ -3,15 +3,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {GoogleOAuthProvider} from '@react-oauth/google'
 import {Provider} from 'react-redux'
 import {store} from './store'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <GoogleOAuthProvider clientId='900378498254-snk01igj6i4dggd9th06babr89tf9s5s.apps.googleusercontent.com'>
     <Provider store={store}>
       <App/> 
       </Provider> 
+      </GoogleOAuthProvider>
   </React.StrictMode>
 );
